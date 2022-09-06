70816038f814ebb61192693fd5106c01b1f4d40e
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-09-06 11:00:12 -0500

    Create Notes01_Printe_Statments.java

---
+++ src/notes/Notes01_Printe_Statments.java
@@ -0,0 +1,65 @@
+package notes;
+
+public class Notes01_Printe_Statments {
+
+	
+	public static void main(String[] args) {
+		
+		//basic print statments
+		
+		//print "Hello" to the console
+		System.out.print("Hello");
+		
+		//problem  it's all on the same line
+		System.out.print("Hello");
+		System.out.print("Hello");
+		System.out.print("Hello");
+		
+		System.out.println();
+		
+		//println()  puts a return after the print
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
+		System.out.print("boo!");
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		/*
+		 * Enter escape sequences
+		 * 
+		 * What if you need to print something that
+		 * isn't a character
+		 * Example: tab, return
+		 * 
+		 * return = \n
+		 * tab = \t
+		 * quotes = \"
+		 * 
+		 */
+		
+		System.out.println("Hey Y'all\nHow's your weekend?");
+		System.out.println("Hey\tblue\tberry\tgreen");//tabs between items
+		
+		System.out.println("\n");
+		
+		System.out.println("\"Toy Story\"");
+		
+		
+		//What if I want a backslash?
+		//put 2 backslashes for every 1 you need
+		System.out.println("\\/");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
